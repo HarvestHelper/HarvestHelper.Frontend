96d9c0a5a92f4979851b1b251409b7ec150b514f
Duarte Somsen <ds@letsgrow.com>
2023-06-06 12:54:04 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,9 @@
-// import defineConfig from 'cypress';
-// import fs from 'fs';
+const { defineConfig } = require('cypress')
 
-// export default defineConfig({
-//     e2e: {
-//         setupNodeEvents(on, config) {
-//             on(
-//                 'after:spec',
-//                 (spec , results) => {
-//                     if (results && results.video) {
-//                         // Do we have failures for any retry attempts?
-//                         const failures = results.tests.some((test) =>
-//                             test.attempts.some((attempt) => attempt.state === 'failed')
-//                         );
-//                         if (!failures) {
-//                             // delete the video if the spec passed and no tests retried
-//                             fs.unlinkSync(results.video);
-//                         }
-//                     }
-//                 }
-//             );
-//         },
-//     },
-// });
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})

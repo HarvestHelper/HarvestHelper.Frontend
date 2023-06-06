90687842b54873c00eeaf76461440befd62f8baf
Duarte Somsen <ds@letsgrow.com>
2023-06-06 11:09:28 +0200

    updte

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,24 @@
-import defineConfig from 'cypress';
-import fs from 'fs';
+// import defineConfig from 'cypress';
+// import fs from 'fs';
 
-export default defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            on(
-                'after:spec',
-                (spec , results) => {
-                    if (results && results.video) {
-                        // Do we have failures for any retry attempts?
-                        const failures = results.tests.some((test) =>
-                            test.attempts.some((attempt) => attempt.state === 'failed')
-                        );
-                        if (!failures) {
-                            // delete the video if the spec passed and no tests retried
-                            fs.unlinkSync(results.video);
-                        }
-                    }
-                }
-            );
-        },
-    },
-});
+// export default defineConfig({
+//     e2e: {
+//         setupNodeEvents(on, config) {
+//             on(
+//                 'after:spec',
+//                 (spec , results) => {
+//                     if (results && results.video) {
+//                         // Do we have failures for any retry attempts?
+//                         const failures = results.tests.some((test) =>
+//                             test.attempts.some((attempt) => attempt.state === 'failed')
+//                         );
+//                         if (!failures) {
+//                             // delete the video if the spec passed and no tests retried
+//                             fs.unlinkSync(results.video);
+//                         }
+//                     }
+//                 }
+//             );
+//         },
+//     },
+// });
